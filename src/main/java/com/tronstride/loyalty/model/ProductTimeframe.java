bd0068f9447c4fff9cf0e493e5da21210bd18498
arjun <arjuns.sambyal@gmail.com>
2024-05-13 18:39:17 +0530

    timeframe

---
+++ src/main/java/com/tronstride/loyalty/model/ProductTimeframe.java
@@ -0,0 +1,48 @@
+package com.tronstride.loyalty.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Table(name = "Timeframe")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+
+public class ProductTimeframe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "start_on")
+    private LocalDate startOn;
+
+    @Column(name = "expires_on")
+    private LocalDate expiresOn;
+
+    @Column(name = "ValidFor")
+    private Boolean validFor;
+
+    @Column(name = "Span")
+    private Integer span;
+
+    @Column(name = "DateCreated", updatable = false)
+    @CreatedDate
+    private LocalDate dateCreated;
+
+    @Column(name = "LastUpdated")
+    @LastModifiedDate
+    private LocalDate lastUpdated;
+
+}
